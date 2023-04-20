e53529097969562a754fa9fd2128ea84edfdbddf
NRanjan-17 <nalinishranjan05@gmail.com>
2023-04-21 05:20:10 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 
 # Device
 export FOX_BRANCH="fox_12.1"
-export DT_LINK="https://github.com/NRanjan-17/device_mido_twrp.git -b fox_12.1"
+export DT_LINK="https://github.com/NRanjan-17/device_mido_twrp.git -b fox_12.1-FBE"
 
 export DEVICE="mido"
 export OEM="xiaomi"
